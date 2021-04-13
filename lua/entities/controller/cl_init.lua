16f079e19e67b9eb9584592f9f34d5365e3c74fa
AlicornLunaa <alicornlunaa@gmail.com>
2021-04-12 21:50:57 -0500

    Started work on the HUD for the controller entity

--- lua/entities/controller/cl_init.lua
+++ lua/entities/controller/cl_init.lua
@@ -1,6 +1,21 @@
 include('shared.lua')
 
+local function DrawScoreboard(pos, ang, scale)
+    cam.Start3D2D(pos, ang, scale)
+        draw.RoundedBox(1, -50, -80, 100, 15, Color(53, 53, 53, 200))
+        draw.DrawText("Boat Soccer", "Trebuchet18", 0, -80, Color(255, 255, 255, 255), TEXT_ALIGN_CENTER)
+        draw.RoundedBox(1, -50, -65, 100, 85, Color(141, 141, 141, 200))
+
+        
+    cam.End3D2D()
+end
+
 function ENT:Draw()
-    self:DrawEntityOutline( 1.0 )
     self:DrawModel()
+
+    local mins, maxs = self:GetModelBounds()
+    local pos = self:LocalToWorld(Vector(0, 0, maxs.z + 4))
+    local ang = self:LocalToWorldAngles(Angle(0, 90, 90))
+
+    DrawScoreboard(pos, ang, 0.5)
 end
--- lua/entities/controller/init.lua
+++ lua/entities/controller/init.lua
@@ -5,24 +5,33 @@ include("shared.lua")
 function ENT:Initialize()
     -- Initialize entities
     self:SetModel("models/props_c17/FurnitureWashingmachine001a.mdl")
-    self:SetModelScale(0.75, 0)
     self:PhysicsInit(SOLID_VPHYSICS)
     self:SetMoveType(MOVETYPE_VPHYSICS)
     self:SetSolid(SOLID_VPHYSICS)
     self:SetUseType(SIMPLE_USE)
 
     -- Initialize members
-
+    self.users = {} -- The list of users actually ready to play
 
     -- Physics initialization
     local phys = self:GetPhysicsObject()
     if (phys:IsValid()) then
+        phys:SetMass(500)
         phys:Wake()
     end
 end
 
 function ENT:Use( activator, caller )
-    return
+    if (activator:IsValid() and activator:IsPlayer()) then
+        if (self.users[activator:SteamID64()] == nil) then
+            self.users[activator:SteamID64()] = true
+            return
+        end
+
+        self.users[activator:SteamID64()] = !self.users[activator:SteamID64()]
+    end
+
+    PrintTable(self.users)
 end
 
 function ENT:Think()
