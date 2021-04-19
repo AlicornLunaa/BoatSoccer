424d3b908bc1ea2e007113bcb9cc3eaa7fc5a397
AlicornLunaa <alicornlunaa@gmail.com>
2021-04-19 17:19:36 -0500

    Removed debug concommand from sv_init

--- lua/boat_soccer/sv_init.lua
+++ lua/boat_soccer/sv_init.lua
@@ -39,26 +39,3 @@ end
 function boat_soccer.StartGame(id)
     boat_soccer.controllers[id].entity:StartGame()
 end
-
--- Debug
-concommand.Add("bs_spawn_goal", function(ply)
-    -- Spawns goal and sets the ball as well
-    local tr = util.TraceLine({
-        start = ply:EyePos(),
-        endpos = ply:EyePos() + ply:EyeAngles():Forward() * 10000,
-        filter = function(ent) if (!ent:IsPlayer()) then return true end end
-    })
-
-    if (!tr.Hit) then return end
-
-    local SpawnPos = tr.HitPos + tr.HitNormal * 16
-
-    local ballEnt = ents.Create("bs_ball")
-    ballEnt:SetPos( SpawnPos )
-	ballEnt:Spawn()
-
-	local goalEnt = ents.Create("bs_goal")
-	goalEnt:SetPos( SpawnPos )
-	goalEnt:Spawn()
-    goalEnt:SetBall(ballEnt)
-end )
