ea4e4e6eb6976e147454c47be5d193b8955fe6b9
AlicornLunaa <alicornlunaa@gmail.com>
2021-04-19 21:49:34 -0500

    Ball now changes color to match the last team that touched it

--- lua/entities/bs_ball/init.lua
+++ lua/entities/bs_ball/init.lua
@@ -1,6 +1,7 @@
 AddCSLuaFile("cl_init.lua")
 AddCSLuaFile("shared.lua")
 include("shared.lua")
+include("boat_soccer/sh_init.lua")
 
 function ENT:Initialize()
     self:SetModel("models/props_phx/misc/soccerball.mdl")
@@ -8,13 +9,26 @@ function ENT:Initialize()
     self:SetMoveType( MOVETYPE_VPHYSICS )
     self:SetSolid( SOLID_VPHYSICS )
 
-    self.bs_buoyancy = 1
+    self.bs_buoyancy = 2
 
     local phys = self:GetPhysicsObject()
     if (phys:IsValid()) then
-        phys:SetMass(10)
+        phys:SetMass(50)
         phys:SetBuoyancyRatio(self.bs_buoyancy)
         phys:Wake()
+    end
+end
+
+function ENT:PhysicsCollide(data, phys)
+    -- Set color of the ball to the last team who touched it
+    if (data.HitEntity.ClassName == "bs_boat") then
+        local team = data.HitEntity.team
+
+        if (team == 0) then
+            self:SetColor(boat_soccer_config.team0)
+        else
+            self:SetColor(boat_soccer_config.team1)
+        end
     end
 end
 
