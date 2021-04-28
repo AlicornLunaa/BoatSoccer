ca09cc5a116ce53dc61c72601eeba03880a23c3b
AlicornLunaa <alicornlunaa@gmail.com>
2021-04-28 12:34:04 -0500

    Added third boat, the fisherman boat

---
+++ lua/entities/bs_boat_fish/init.lua
@@ -0,0 +1,17 @@
+-- Client file initialization
+AddCSLuaFile("cl_init.lua")
+AddCSLuaFile("shared.lua")
+
+-- Include entity settings
+include("shared.lua")
+include("boat_soccer/sh_init.lua")
+
+-- Entity functions
+function ENT:Initialize()
+    -- Initialize entity
+    self:SetModel("models/boat_soccer/fishermanboat.mdl")
+    self:InitializeData()
+    self.offset = Angle(0, 0, 0)
+    self.bs_buoyancy = 3
+    self:InitPhys()
+end
